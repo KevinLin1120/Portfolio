52c32300f710122f4bb11beb12248a9f28166c72
KevinLin1120 <80439584+KevinLin1120@users.noreply.github.com>
2025-01-22 23:05:20 +0800

    update index page
    - hide scrollbar in index
    - remove useless codes
    - replace slides in index

--- js/index.js
+++ js/index.js
@@ -1,60 +1,54 @@
-// Intersection observer
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach((entry) => {
-        // show
-        if(entry.isIntersecting && entry.target.className.match("hidden-works")){
-            entry.target.classList.remove('hidden');
-            entry.target.classList.add('hidden-works-show');
-            entry.target.classList.remove('hidden-works-hidden');
-
-            console.log(menu);
-            menu.classList.add("floatToBottom");
-        }
-        // hidden
-        else if(entry.target.className.match("hidden-works")){
-            entry.target.classList.add('hidden');
-            entry.target.classList.add('hidden-works-hidden');
-            entry.target.classList.remove('hidden-works-show');
-
-            menu.classList.remove("floatToBottom");
-        }
-    });
-});
-
 // Change name when hover
 let Name = document.getElementById('name');
-Name.onmouseover = ()=>{
+Name.onmouseover = () => {
     Name.innerHTML = "林俊廷";
-    
+
 };
-Name.onmouseleave = ()=>{
+Name.onmouseleave = () => {
     Name.innerHTML = "Kevin";
 };
 
 // Skills 
 let collapseBtns = document.getElementsByClassName('btn-check');
-for(let i = 0; i < collapseBtns.length; i++){
+for (let i = 0; i < collapseBtns.length; i++) {
     // When click on the btn
-    collapseBtns[i].addEventListener('click', ()=>{
+    collapseBtns[i].addEventListener('click', () => {
         // Turn off other collapse
         let collapses = document.getElementsByClassName('collapse-horizontal');
-        for(let j = 0; j < collapses.length; j++){
-            if(i != j){
+        for (let j = 0; j < collapses.length; j++) {
+            if (i != j) {
                 collapses[j].classList.remove('show');
             }
         };
     });
 }
 
-// Selected works show up
-// const observer = new IntersectionObserver((entries)=>{
-//     entries.forEach
-// })
-
 // Selected works 
-const hiddenWorks = document.querySelectorAll('.hidden-works');
-hiddenWorks.forEach(element => {
-    observer.observe(element);
-});
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 10,
+    centeredSlides: true, // Ensures the central slide is always centered
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 0, // No rotation for the slides
+        stretch: 0, // No stretching of the slides
+        depth: 150, // Depth perspective effect
+        modifier: 1, // Multiplies the depth and scale effects
+        slideShadows: false, // Disables slide shadows for cleaner look
+    },
+    autoplay: {
+        delay: 3000,
+    },
 
-let menu = document.querySelector('.menu');
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    }
+});
